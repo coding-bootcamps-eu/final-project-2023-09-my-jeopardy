3678e005ef6f43dcebafdcefa708f2b1c8a38fd6
Torben Sievertsen <torben.sievertsen27@gmail.com>
2023-12-20 13:32:31 +0100

    created QuizTemplateView.vue

---
+++ src/components/PlayerStats.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="player-container">
+    <div class="player" :key="index" v-for="(Player, index) in PlayerName">
+      <h2>{{ Player }}</h2>
+      <h3>{{ Points[index] }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      PlayerName: JSON.parse(localStorage.getItem('playerNames')) || ['Player 1', 'Player 2'],
+      Points: JSON.parse(localStorage.getItem('points')) || [0, 0]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.player-container {
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+}
+.player {
+  margin: auto;
+}
+</style>
