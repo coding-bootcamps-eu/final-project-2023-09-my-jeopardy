c3a1f85876c69ab14e9d5e596c90efed9a7fc068
Torben Sievertsen <torben.sievertsen27@gmail.com>
2023-12-21 10:31:20 +0100

    Added styling to Home screen

--- src/views/HomeScreenView.vue
+++ src/views/HomeScreenView.vue
@@ -13,14 +13,23 @@
           >Play Quiz</v-btn
         >
       </router-link>
-      <v-btn>Create Quiz</v-btn>
+      <v-btn class="btn">Create Quiz</v-btn>
     </div>
   </body>
 </template>
-
+88b7c6 efd0be
 <style scoped>
+@import url(https://fonts.bunny.net/css?family=akaya-kanadaka:400);
+* {
+  --primary-color: #f1e0dd;
+  --primary-background-color: #88b7c6;
+  --secondary-background-color: #589bac;
+}
 template,
 body {
+  font-family: 'Akaya Kanadaka', display;
+  background-color: var(--primary-background-color);
+  color: var(--primary-color);
   height: 100%;
   display: flex;
   flex-direction: column;
@@ -40,8 +49,8 @@ h1 {
   width: 30%;
 }
 .btn {
+  font-family: 'Akaya Kanadaka', display;
   margin-bottom: 2rem;
-  text-decoration: none;
-  color: inherit;
+  background-color: var(--secondary-background-color);
 }
 </style>
