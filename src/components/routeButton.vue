d9c0c1d9def541838f950c3f07461387a9bf1b47
Torben Sievertsen <torben.sievertsen27@gmail.com>
2023-12-21 08:58:49 +0100

    created route Button Component

---
+++ src/components/routeButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-link :to="route" custom v-slot="{ navigate }">
+    <v-btn @click="navigate" :v-text="description"></v-btn>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    route: String,
+    description: String
+  }
+}
+</script>
